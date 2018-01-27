5a199a68cc938af8dddde3c1a4da91f283341055
bbaginski <excaliber2x@gmail.com>
2018-01-27 16:46:51 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function startGame(){
+    var username = prompt("What is Your Name?");
+    var HP = 10;
+    var xHP = 10;
+    var xlives = 3;
+    var gameover = false;
+    while (HP > 0){
+      var damage = getRandomInt(3);
+      HP -= damage;
+      console.log(`Grant attacks and does ${damage} damage, ${username} HP is at ${HP}`)
+      damage = getRandomInt(3);
+      xHP -= damage;
+      console.log(`${username} attacks grant, it does ${damage} damage, his HP is at ${xHP}`)
+      if (xHP <= 0 && xlives === 0){
+        console.log('YOU HAVE DEFEATED GRANT!');
+        break;
+      }
+      else if (xHP <= 0 ){
+        xlives--;
+        xHP = 10;
+        console.log('GRANT HAS HEALED HIMSELF!');
+      } 
+      
+      
+    }
+    console.log('GAME OVER')
+    
+  }
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
